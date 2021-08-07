7e3324b626d66a4a2656baa46e5f560f91b9b32e
karim tarek <karimtarekabdelaziz@gmail.com>
2021-08-07 23:29:50 +0200

    update project 8/8

---
+++ components/UserInfo.vue
@@ -0,0 +1,53 @@
+<template>
+  <!--  -->
+  <div class="d-flex">
+    <!--  -->
+    <figure class="width-61 height-55 radius-21" v-on="$listeners">
+      <img
+        role="image-user"
+        class="img-fluid"
+        src="~/assets/images/global/avatar.png"
+        alt="avatar"
+      />
+    </figure>
+    <!--  -->
+    <div
+      role="info-user"
+      class="text-12 text-black d-flex flex-column align-items-center"
+    >
+      <div class="weight-br-300">
+        <span v-if="statusWelcome" role="welcome">أهلا</span>
+        <span role="name-user" v-text="nameUser" />
+      </div>
+      <span
+        role="email-user"
+        class="text-silver weight-br-300 margin-top-5"
+        v-text="email"
+      />
+    </div>
+    <!--  -->
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo',
+  props: {
+    nameUser: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      default: '',
+    },
+    statusWelcome: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
+
+<style></style>
