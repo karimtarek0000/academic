3c135520ef1a9ebe7023ae678a486e4d308b006a
karimtarek <43219770+karimtarek0000@users.noreply.github.com>
2023-01-27 22:35:14 +0200

    Update project Nuxt 2

--- nuxt.config.js
+++ nuxt.config.js
@@ -197,18 +197,4 @@ export default {
       })
     },
   },
-
-  build: {
-    extend(config, ctx) {
-      config.module.rules.push({
-        enforce: 'pre',
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        exclude: /(node_modules)/,
-        options: {
-          fix: true,
-        },
-      })
-    },
-  },
 }
